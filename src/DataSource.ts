dac88512f40a75d1a54ac851414696faf2886551
jfa <git@jonathanallenberg.de>
2021-05-17 10:33:54 +0200

    …

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -3,17 +3,9 @@ import Stripe from 'stripe'
 
 type StripeParams = ConstructorParameters<typeof Stripe>
 
-export class StripeDataSource extends DataSource {
-  client: Stripe
-  private initClient(...params: StripeParams) {
-    this.client = new Stripe(...params)
+export class StripeDataSource extends Stripe implements DataSource {
+  constructor(...params: StripeParams) {
+    super(...params)
   }
-  constructor(...params: StripeParams | []) {
-    super()
-    if (typeof params[0] === 'string') {
-      this.initClient(...(params as StripeParams))
-    }
-  }
-
   initialize() {}
 }
